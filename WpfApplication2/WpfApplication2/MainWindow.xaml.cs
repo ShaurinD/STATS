6fdfa30077c0c23461f834eef6b222d10a0ac30d
Arun Kuriakose <kuriakose.arun@gmail.com>
2013-10-09 19:08:34 -0400

    Keyboard

---
+++ WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class AlphaButton : Button
+    {
+        public AlphaButton(TextBox tb)
+        {
+            this.Width = 80;
+            this.Height = 72;
+            this.HorizontalAlignment = HorizontalAlignment.Left;
+            this.VerticalAlignment = VerticalAlignment.Top;
+            this.tb = tb;
+        }
+
+        public void AlphaButton_Click(object sender, RoutedEventArgs e)
+        {
+            tb.AppendText(this.Content.ToString());
+        }
+
+        private TextBox tb;
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Keyboard.Visibility = Visibility.Collapsed;
+        }
+    }
+}
