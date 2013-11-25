bf6a7ff5ddc56e1de4e8583a4e9dea26d9f481d0
Sam <samuelwu@umich.edu>
2013-11-24 23:30:26 -0500

    o.O

--- GazeDectect/GazeDectect/GazeDectect.cpp
+++ GazeDectect/GazeDectect/GazeDectect.cpp
@@ -64,23 +64,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	gab_size.width=640;
 	image=cvCreateImage(gab_size,8,3);
 
-	IplImage *depth=0;
-	CvSize gab_size_depth;
-	gab_size_depth.height=240;
-	gab_size_depth.width=320;
-	depth=cvCreateImage(gab_size,8,1);
-
 	PXCImage::ImageData idata;
-	PXCImage::ImageData data_depth;
 
 	unsigned char *rgb_data;//=new unsigned char[];
-	float *depth_data;
 	PXCImage::ImageInfo rgb_info;
-	PXCImage::ImageInfo depth_info;
 
-	cvNamedWindow("depth_cv2",0);
-	cvResizeWindow("depth_cv2",320,240);
-
+	Mat colorMat;
+	namedWindow("Test");
+	namedWindow("Test2");
 	///////
 	for (;;) {
 		capture.ReadStreamAsync(images.ReleaseRefs(),sps.ReleaseRef(0)); 
@@ -89,18 +80,11 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		if (!pipeline.AcquireFrame(true)) break;
 		PXCImage *color_image=pipeline.QueryImage(PXCImage::IMAGE_TYPE_COLOR);
-		PXCImage *depth_image=pipeline.QueryImage(PXCImage::IMAGE_TYPE_DEPTH );
 
 		color_image->AcquireAccess(PXCImage::ACCESS_READ_WRITE,PXCImage::COLOR_FORMAT_RGB24,&idata); 
-		depth_image->AcquireAccess(PXCImage::ACCESS_READ,&data_depth);
 
 		rgb_data=idata.planes[0];
-		depth_data=(float*)data_depth.planes[0];
 
-
-		pxcStatus stat1 = depth_image->QueryInfo(&depth_info);
-		int w1=depth_info.width;
-		int h1=depth_info.height;
 
 		for(int y=0; y<480; y++)
 		{
@@ -113,31 +97,15 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 		}
 
-		for(int y=0; y<240; y++)
-		{
-			for(int x=0; x<320; x++)
-			{ 
-				depth->imageData[y*320+x]=depth_data[y*320+x]/16;
-			}
-		}
-
 		color_image->ReleaseAccess(&idata);
-		depth_image->ReleaseAccess(&data_depth);
-
+		/*
 		cvShowImage("rgb_cv", image);
 		cvShowImage("depth_cv2", depth);
-
-		/*
-		Mat colorMat(image);
-
-		imshow("Shaurin Sucks", colorMat);
 		*/
-		
-		if( cvWaitKey(10) >= 0 )
-		break;
-		//IF THIS IS AFTER. THEN THE STREAM STOPS IF FACE ISNT IN THERE
-		if (!depth_render.RenderFrame(depth_image)) break;
-		pipeline.ReleaseFrame();
+		colorMat = image;
+
+		//imshow("ShaurinSucks", colorMat);
+
 
 
 		// Tracking or recognition results are ready 
@@ -147,18 +115,57 @@ int _tmain(int argc, _TCHAR* argv[])
 		landmark->QueryLandmarkData(fid, linfo.labels, &ldata[0]);
 		landmark->QueryPoseData(fid, &pdata);
 		det->QueryData(fid,&data); 
-		cout << "DATA   " << data.confidence << " " << data.viewAngle << " " << data.rectangle.x << " " << data.rectangle.y << endl;
-		cout << "PDATA  " << pdata.pitch << " " << pdata.roll << " " << pdata.yaw << endl;
+		//cout << "DATA   " << data.confidence << " " << data.viewAngle << " " << data.rectangle.x << " " << data.rectangle.y << endl;
+		//cout << "PDATA  " << pdata.pitch << " " << pdata.roll << " " << pdata.yaw << endl;
 						
-		cout << "LDATA ";
+		//cout << "LDATA ";
 		for (int j = 0; j < 7; j++){
-			cout << ldata[j].position.x << " " << ldata[j].position.y << " " << ldata[j].position.z << endl;
+			//cout << ldata[j].position.x << " " << ldata[j].position.y << " " << ldata[j].position.z << endl;
 		}
+
+		//Point center( ldata[6].position.x, ldata[6].position.y);
+		//ellipse(colorMat, center, Size( 10, 10), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+
+
+		cout << ldata[0].position.x << " " << ldata[0].position.y << " | " << ldata[1].position.x << " " << ldata[1].position.y << endl;
+		//Point2f start1(ldata[0].position.x, ldata[0].position.y);
+		//Point2f end1(ldata[1].position.x, ldata[1].position.y);
+
+		//Point2f start2(ldata[2].position.x, ldata[2].position.y);
+		//Point2f end2(ldata[3].position.x, ldata[3].position.y);
+
+		//int rad = abs((ldata[0].position.x+ldata[1].position.x)/2 - ldata[0].position.x);
+
+		//Point2f mid1((ldata[0].position.x+ldata[1].position.x)/2, (ldata[0].position.y+ldata[1].position.y)/2);
+		//ellipse(colorMat, mid1, Size( 50, 50), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+
+		Point2f corner1(ldata[0].position.x, ldata[0].position.y+10);
+		Point2f corner2(ldata[1].position.x, ldata[1].position.y-10);
+
+		//rectangle(colorMat, corner1, corner2, Scalar(0, 255, 255), 2, 8);
+
+		Rect temp(corner1, corner2);
+		//colorMat(temp);
+
+
+		//line(colorMat, start1, end1, Scalar(255,0,255), 2, 8);
+		//line(colorMat, start2, end2, Scalar(255,0,255), 2, 8);
+
+		imshow("Test", colorMat);
+		Mat frame_gray;
+		cvtColor(colorMat, frame_gray, CV_BGR2GRAY);
+		equalizeHist(frame_gray, frame_gray);
+		Mat faceROI = frame_gray(temp);
+		imshow("Test2", faceROI);
+				
+		if( cvWaitKey(10) >= 0 )
+		break;
+		//IF THIS IS AFTER. THEN THE STREAM STOPS IF FACE ISNT IN THERE
+		pipeline.ReleaseFrame();
 
 	}
 	
 	cvReleaseImage(&image);
-	cvReleaseImage(&depth);
 	pipeline.Close();
 	return 0;
 }
